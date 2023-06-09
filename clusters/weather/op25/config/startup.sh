62e5a5e459efda1e8895471085c372c4be701ae1
David Olsen <dolsen@handy.com>
2023-06-08 22:09:09 -0600

    point at udplistener service

--- clusters/weather/op25/config/startup.sh
+++ clusters/weather/op25/config/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /src/op25/op25/gr-op25_repeater/apps
-python3 ./rx.py --nocrypt --args "rtl" --gains 'lna:36' -S 960000 -X -q 0 -v 1 -2 -V -l http:0.0.0.0:8091 -w -W 172.16.10.5 -u 23456 -T trunk.tsv
+python3 ./rx.py --nocrypt --args "rtl" --gains 'lna:36' -S 960000 -X -q 0 -v 1 -2 -V -l http:0.0.0.0:8091 -w -W udplistener.dtrs-radio -u 23456 -T trunk.tsv
