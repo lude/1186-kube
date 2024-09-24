2ee8720f13af13bc001e88fe897116c406c7aa5d
David.Olsen <david.olsen@angi.com>
2024-09-24 16:02:07 -0600

    change the destination hostnames for the two sdr apps

--- clusters/weather/op25/config/startup.sh
+++ clusters/weather/op25/config/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /src/op25/op25/gr-op25_repeater/apps
-python3 ./rx.py --nocrypt --args "rtl=1" --gains 'lna:36' -S 960000 -X -q 0 -v 1 -2 -V -l http:0.0.0.0:8091 -w -W udplistener.dtrs-radio -u 23456 -T trunk.tsv
+python3 ./rx.py --nocrypt --args "rtl=1" --gains 'lna:36' -S 960000 -X -q 0 -v 1 -2 -V -l http:0.0.0.0:8091 -w -W udplistener-dtrs.udplistener -u 23456 -T trunk.tsv
