f4344e50ae32919702374f6026c9e1b1ed7a1829
Dave Olsen <m@lude.net>
2025-03-01 21:36:32 -0700

    trying to migrate to multi_rx

--- clusters/weather/op25/config/startup.sh
+++ clusters/weather/op25/config/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd /src/op25/op25/gr-op25_repeater/apps
-python3 ./rx.py --nocrypt --args "rtl=1" --gains 'lna:36' -S 960000 -X -q 0 -v 1 -2 -V -l http:0.0.0.0:8091 -w -W udplistener-dtrs.udplistener -u 23456 -T trunk.tsv
+#python3 ./rx.py --nocrypt --args "rtl=1" --gains 'lna:36' -S 960000 -X -q 0 -v 1 -2 -V -l http:0.0.0.0:8091 -w -W udplistener-dtrs.udplistener -u 23456 -T trunk.tsv
+python3 ./multi_rx.py -v 1 -c colorado_dtrs.json 2>&1
